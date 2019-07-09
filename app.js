e91439b44bac7bb5d1463edd8cf791d47f0d35cd
mahsum <mahsum@webjump.com.br>
2019-07-08 22:37:26 -0300

    fix graphql start error

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+  
 const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
@@ -7,31 +8,32 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/graphql', graphqlHttp({
+app.use(
+  '/graphql',
+  graphqlHttp({
     schema: buildSchema(`
         type RootQuery {
             events: [String!]!
         }
-
         type RootMutation {
             createEvent(name: String): String
         }
-
         schema {
             query: RootQuery
             mutation: RootMutation
         }
     `),
     rootValue: {
-        events: () => {
-            return ['Romantic Cooking', 'Sailing', 'All-night Codig']
-        },
-        createEvent: (args) => {
-            const eventName = args.name;
-            return eventName;    
-        },
-        graphiql: true
-    }
-}));
+      events: () => {
+        return ['Romantic Cooking', 'Sailing', 'All-Night Coding'];
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      }
+    },
+    graphiql: true
+  })
+);
 
-app.listen(3001);
+app.listen(3000);
